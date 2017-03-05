1a7b819b6165d1f774a62b986256e48a4ec4f38c
Sherry Umlah <sumlah@articulate.com>
2017-03-05 11:44:50 -0600

    Create default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -72,3 +72,6 @@ gulp.task('compass', function(){
     .on('error', gutil.log)
     .pipe(gulp.dest('builds/development/css'))
 });
+
+// Gulp Task to Run all as dependency tasks
+gulp.task('default', ['coffee','js','compass']);
